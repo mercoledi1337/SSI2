bfd899fbab9d9cf16667743271ded5bd71035682
Michał Sereda <mercoledics@gmail.com>
2025-11-27 22:34:42 +0100

    warping around variable when they reaching borders

--- main.cpp
+++ main.cpp
@@ -2,23 +2,17 @@
 
 #include <iostream>
 #include <fstream>
-#include <ctime>
-#include <random>
-#include <cmath>
 #include "gnuplot_iostream.h"
-#include <numeric>
-#include <cstdlib>
-
-
 #include <stdlib.h>
+
 using namespace std;
 
 //range from 0 to 100
-const int randMIN = 15;
+const int randMIN = 0;
 const int randMINrange = 0;
-const int randMAX = 36;
-const int randMAXrange = 101;
-const int iter = 20;
+const int randMAX = 100;
+const int randMAXrange = 100;
+const int iter = 100;
 
 
 
@@ -29,9 +23,9 @@ int main() {
 
     Gnuplot gp("D:/gnuplot/bin/gnuplot.exe");
 
-    double x = static_cast<double>(numberX) / (static_cast<double>(UINT_MAX) + 1) * (randMAX - randMIN) + randMIN;
+    double x = (double)(numberX) / ((double)(UINT_MAX) + 1) * randMAX + randMIN;
     double y = sin(x/10) * sin(x/200);
-    double border = 21; //random in range 20;
+    double border = 10; //random in range 20;
 
     std::vector<std::pair<double,double>> points;
 
@@ -39,22 +33,14 @@ int main() {
 
     for (int i = 0; i < iter; i++) {
         rand_s(&number);
-
-        double randInBorder = static_cast<double>(number) / (static_cast<double>(UINT_MAX) + 1) * border - border/2.0; // chossing number from 0 to 20 when is belove 10 means is negativ so he have to add -10
+        double randInBorder = border * ( (double)number / (double)UINT_MAX * 2.0 - 1.0 ); //choosing betwen -1 1 and then multiply by border
         double xpot = x + randInBorder;
-        while (xpot < randMIN) {
-            xpot += 0.1;
-        }
-        while (xpot > randMAX) {
-            xpot -= 0.1;
-        }
+        while (xpot < randMIN) xpot += (randMAX - randMIN);
+        while (xpot > randMAX) xpot -= (randMAX - randMIN);
         double ypot = sin(xpot/10) * sin(xpot/200);
 
         // printing values in iteration
-        std::cout << "Iteracja: " << i + 1
-                  << "  x=" << xpot
-                  << "  y=" << ypot
-                  << "  rozrzut=" << border << "\n";
+        std::cout << "Iteracja: " << i + 1 << "  x=" << xpot << "  y=" << ypot << "  rozrzut=" << border << "\n";
 
         if (ypot >= y) {
             y = ypot;
@@ -66,7 +52,6 @@ int main() {
 
         // saving poing into vector for gnuplot
         points.emplace_back(xpot, ypot);
-
 
         gp << "plot "
               "sin(x/10)*sin(x/200) w l lw 2 title 'Funkcja', "
